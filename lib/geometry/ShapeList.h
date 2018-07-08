9c84809f58ae34e7e998b1e97733c19188b49e8f
Matthew Westrik <mattw@squareup.com>
2018-07-08 13:13:27 -0400

    ShapeLists are Shapes

--- lib/geometry/ShapeList.h
+++ lib/geometry/ShapeList.h
@@ -6,12 +6,12 @@
 
 namespace Millipede {
 
-class ShapeList {
+class ShapeList : public Shape {
     std::vector<std::shared_ptr<Shape > > shape_list;
 public:
     ShapeList() {}
     ~ShapeList() {}
-    ShapeList(std::vector<std::shared_ptr<Shape > > list) { shape_list = list; };
+    ShapeList(std::vector<std::shared_ptr<Shape > > list) : shape_list(list) {};
     virtual bool hit(const Ray& r, double t_min, double t_max, hit_record& hit_record) const;
 };
 
